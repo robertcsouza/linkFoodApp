316dee2492067fb26ba32e71c41c4cb5c3662d21
dreadPireRob <usuario@MacBook-Air-de-Usuario.local>
2022-02-21 17:23:25 -0400

    initialize login screen

---
+++ lib/assets/Colors.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+
+final Color gray = Color(0XFFE8E8E8);
---
+++ lib/components/Inputs.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import '../assets/Colors.dart' as projectColor;
+
+Widget input({TextEditingController? controller, String? lable, context}) {
+  return Padding(
+    padding: const EdgeInsets.only(top: 8.0, left: 16.0, right: 16.0),
+    child: Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      decoration: BoxDecoration(
+          color: projectColor.gray,
+          borderRadius: BorderRadius.all(Radius.circular(10))),
+      child: TextField(
+        controller: controller,
+        decoration: InputDecoration(
+            border: InputBorder.none,
+            hintText: lable,
+            contentPadding: EdgeInsets.only(left: 20),
+            hintStyle: TextStyle()),
+      ),
+    ),
+  );
+}
